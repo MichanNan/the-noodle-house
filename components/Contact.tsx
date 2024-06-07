4e302be9143b2e6546f9cfc043a2474b43006578
Nan <caonanla@gmail.com>
2024-06-07 23:47:40 +0200

    v 1.0.1

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -20,7 +20,7 @@ const Contact = () => {
           <div className="flex flex-col">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2369.6735615648386!2d10.021485277113849!3d53.56359455844577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b18f662626a9a1%3A0x8bcc10eae9105c54!2sThe%20noodle%20house!5e0!3m2!1szh-CN!2sde!4v1717449839909!5m2!1szh-CN!2sde"
-              width="600"
+              width="450"
               height="450"
               loading="lazy"
             ></iframe>
