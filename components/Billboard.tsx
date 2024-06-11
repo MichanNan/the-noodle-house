e252a137c4dedd5e6b798de2400d5fafe3a14e05
Nan <caonanla@gmail.com>
2024-06-11 12:44:51 +0200

    add ins

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { FaSquareInstagram } from "react-icons/fa6";
 
 const Billboard = () => {
   return (
@@ -19,18 +21,27 @@ const Billboard = () => {
             alt="logo"
             className="mt-10"
           />
-          <p className="md:text-xl text-green-800 mt-6 m-4 text-left">
-            Nudeln haben in China eine lange Geschichte, und jede Region hat
-            ihre eigenen charakteristischen Nudelgerichte. Unser Restaurant
-            konzentriert sich darauf, die beliebtesten, bissfesten Nudeln aus
-            dem Norden Chinas anzubieten, ergänzt durch eine Vielzahl von
-            Toppings und eine köstliche Brühe, die stundenlang aus Knochen
-            gekocht wird. Unsere Nudeln sind aromatisch und lecker, ohne den
-            Zusatz von Glutamat. Außerdem bieten wir eine Auswahl an Pilzbrühe
-            für Vegetarier an, um unterschiedliche Geschmacksrichtungen zu
-            befriedigen. Wir laden Sie herzlich ein, die echten chinesischen
-            Nudeln zu probieren.
-          </p>
+          <div className="flex flex-col items-start justify-start gap-4">
+            <p className="md:text-xl text-green-800 mt-6 m-4 text-left">
+              Nudeln haben in China eine lange Geschichte, und jede Region hat
+              ihre eigenen charakteristischen Nudelgerichte. Unser Restaurant
+              konzentriert sich darauf, die beliebtesten, bissfesten Nudeln aus
+              dem Norden Chinas anzubieten, ergänzt durch eine Vielzahl von
+              Toppings und eine köstliche Brühe, die stundenlang aus Knochen
+              gekocht wird. Unsere Nudeln sind aromatisch und lecker, ohne den
+              Zusatz von Glutamat. Außerdem bieten wir eine Auswahl an Pilzbrühe
+              für Vegetarier an, um unterschiedliche Geschmacksrichtungen zu
+              befriedigen. Wir laden Sie herzlich ein, die echten chinesischen
+              Nudeln zu probieren.
+            </p>
+            <div className="ml-4 flex justify-center items-center gap-4 text-green-800">
+              {" "}
+              <Link href="https://www.instagram.com/thenoodlehouse_hh/">
+                <FaSquareInstagram color="gray" size={30} />
+              </Link>
+              <p>Folgen Sie uns auf Instagram</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
