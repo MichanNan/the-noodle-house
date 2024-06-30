0cbcfa983743bdaedd493e0f9dc32b960386a8be
Nan <caonanla@gmail.com>
2024-07-01 00:20:14 +0200

    change time

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -16,8 +16,10 @@ const Contact = () => {
             <p>Tel:+49 40 22759255</p>
             <p>Email:Info@the-noodle-house.de</p>
             <p className="mt-4">Open:</p>
-            <p>Mo-Fr 12:30-15:00, 17:00-21:00</p>
-            <p>Sa-So 12:30-21:00</p>
+            <p>Di-Mi 17:00-21:00</p>
+            <p>Do-Fr 12:00-15:00 17:00-21:00</p>
+            <p>Sa-So 12:00-21:00</p>
+            <p>Mo geschlossen</p>
           </div>
           <div className="flex flex-col">
             <iframe
